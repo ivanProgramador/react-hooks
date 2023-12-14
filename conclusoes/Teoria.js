e2cd475812ec523852e398a873e7b7f6cf2c23c0
ivan <ivan-guita@hotmail.com>
2023-12-13 21:10:40 -0300

    Aula 1 definição basica de hooks

---
+++ conclusoes/Teoria.js
@@ -0,0 +1,39 @@
+/*
+
+ componetes de função são interessantes pra exbir retornos estaticos
+ mas os componentes de classe possuem mais possibilidades como por exemplo
+ gerenciamento de estado.
+ 
+ Os hooks permitem que possaos usar recursos de um componete de classe mesmo
+ que os componetes escritos sejam componentes de função.
+ 
+ SINTAXE 
+   use<nome do hook>() por exeplo um hook que pode ser usado para gerenciar 
+   o estado de um componente useState().
+
+- Hooks são funções que te possibilitam ligar se a recursos de state e ciclo de vida de um componente
+- Hooks não funcionan dentro de classe eles permitem que voce usae recursos de classe
+- Hooks são 100% retrocompativeis não tem restrição de versões pra uso
+
+O react fornece alguns hooks 
+
+basicos 
+- useState()
+- useEfect()
+- useContext()
+
+Adicionais 
+- useReducer()
+- useCallback()
+- useMemo()
+- useRef()
+- useImperativeHandle()
+- useLayoutEffect()
+  
+
+
+
+
+
+ 
+*/
